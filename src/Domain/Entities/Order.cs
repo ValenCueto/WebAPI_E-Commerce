bd05aec346a87a1e678da68408aaef392a2ea2ac
Matias D'Anunzio <matidanunzio@gmail.com>
2024-09-18 15:20:19 -0300

    Classes added

---
+++ src/Domain/Entities/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public List<Product> Products { get; set; }
+        public float TotalPrice { get; set; }
+        public Client Client { get; set; }
+    }
+}
---
+++ src/Domain/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    abstract public class User
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public RolEnum Rol { get; set; }
+    }
+}
